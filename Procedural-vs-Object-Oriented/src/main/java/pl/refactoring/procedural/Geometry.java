3edea57bd1f129a25472f1f0d4dc91f081b770fc
wlodekkr <wlodzimierz.krakowski@gmail.com>
2022-02-20 13:25:59 +0100

    Refactoring VLog Series

---
+++ Procedural-vs-Object-Oriented/src/main/java/pl/refactoring/procedural/Geometry.java
@@ -0,0 +1,24 @@
+package pl.refactoring.procedural;
+
+/**
+ * Sources provided under freeware licence
+ * Wlodek Krakowski - www.refactoring.pl
+ */
+public class Geometry {
+    public static double area(Object shape){
+        if (shape instanceof Circle){
+            Circle circle = (Circle) shape;
+            return Math.PI * circle.radius * circle.radius;
+        }
+        if (shape instanceof Rectangle){
+            Rectangle rectangle = (Rectangle) shape;
+            return rectangle.width * rectangle.height;
+        }
+        if (shape instanceof Square){
+            Square square = (Square) shape;
+            return square.size * square.size;
+        }
+
+        throw new IllegalArgumentException("Unknown shape provided");
+    }
+}
